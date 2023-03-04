d9fb2f6e8c5c4715f02ef561841730d9de27f909
david medina <rafaelsanchez97.dm@gmail.com>
2023-03-03 21:57:29 -0300

    conexion arreglada a mongodb

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cors from 'cors';
-
-dotenv.config({path: 'back/config.env'});
-
-
 import connectDB from './mongodb/connect.js'
+dotenv.config();
 
 const app = express();
-console.log(process.env.MONGODB_URL)
 app.use(cors());
 app.use(express.json({limit: '50mb'}));//en este caso ese limite es para las fotos que traiga dall-e
 
 
-app.get('/',async(req,res) =>{
+app.get('/', async(req, res) =>{
   res.send('hola mundo');
 });
 
 //const database = process.env.MONGODB_URL;
-const startServer = async() =>{
+const startServer = () =>{
 
   try{
-   await connectDB(process.env.MONGODB_URL);
+    connectDB(process.env.MONGODB_URI);
     app.listen(8000, ()=> console.log(`server iniciado en el puerto ${8000}`))
   } catch (error){
     console.log(error)
--- mongodb/connect.js
+++ mongodb/connect.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () =>{
+const connectDB =  (url) =>{
   mongoose.set('strictQuery', true); //esta parte de codigo es para poder hacer consultas, posiblemente con regex
 
- await mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true} )
-  .then(() => console.log('mongodb conectado'))
-  .catch((err) => console.log(err))
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+    }).then((con) => console.log(`mongodb conectado,host: ${con.connection.host}`))
+    .catch((err) => console.log(err))
 }
 
 export default connectDB;
